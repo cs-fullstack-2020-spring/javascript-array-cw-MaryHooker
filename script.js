6019dd93ca47794df74fac5684d18efe16063744
MaryHooker <mc020791@yahoo.com>
2020-02-19 14:53:33 -0600

    Problems 1&2

---
+++ script.js
@@ -0,0 +1,51 @@
+// // ### Exercise 1
+
+// // - Create the following array ```["John", "Paul", "George", "Pete"]```
+// // - Console.log ONLY Pete’s name from the list
+// // - Change Pete’s name to ‘Ringo’
+// // - Console.log the list
+// // - Add the name ‘Yoko’ to the list
+// // - Console.log the list
+//*********************/
+// // 1.
+// //CREATING ARRAY
+// let beatles = ["John", "Paul", "George", "Pete"];
+// //LOGGING ONLY PETE'S NAME
+// console.log(beatles[3]);
+// //CHANGE PETE TO RINGO
+// beatles[3]= "Ringo";
+// //PRINT THE NEW ARRAY
+// console.log(beatles)
+// //ADD YOKO TO END OF THE LIST
+// beatles.push("Yoko");
+// //PRINT THE NEW ARRAY
+// console.log(beatles);
+
+//****************************************************/
+
+// // ### Exercise 2
+// // - Write a program that asks the user to enter 4 ages. Save each of the ages into an array.
+// // - Console.log the list of ages
+// // - Delete the third age.
+// // - Change the second element (NOT index 2, the second element) to a different age.
+// // - Console.log the list of ages
+// //**********************/
+// // 2.
+// //CREATE PROMPTS TO STORE EACH AGE GIVEN IN A VARIABLE
+// let userAge1 = prompt("Please enter an age");
+// let userAge2 = prompt("Please enter another age");
+// let userAge3 = prompt("Please enter another age");
+// let userAge4 = prompt("Please enter another age");
+// //CREATE ARRAY THAT STORES EACH VALUE GIVEN 
+// let arrayOfAges = [userAge1,userAge2,userAge3,userAge4];
+// //PRINT ARRAY IN CONSOLE
+// console.log(arrayOfAges);
+// //DELETE THE 3RD AGE
+// arrayOfAges.splice(2,1)
+// //CHANGE THE SECOND ELEMENT TO A DIFFERENT AGE
+// arrayOfAges[1]= 5;
+// //LOG THE FINAL LIST OF AGES
+// console.log(arrayOfAges)
+
+//*****************************************************/
+
